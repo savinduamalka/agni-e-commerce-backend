689f6a86cc6b43351f1474e20241c600ca4bfcd6
savinduamalka <savinduamalka@gmail.com>
2025-04-04 20:28:49 +0530

    connect with the database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app= express();
+
+dotenv.config();
+
+const connection = process.env.MONGODB_URL;
 
 const PORT=3000;
 
@@ -11,6 +17,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is Running on ${PORT}`);
 });
+
+mongoose.connect(connection).then(
+  ()=>{
+    console.log("Database connected successfully");
+  }
+).catch(
+  ()=>{
+    console.log("Database connection failed");
+  }
+)
 
 export default app;
 
