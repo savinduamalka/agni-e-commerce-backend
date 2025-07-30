0520d4322bba961c334e94de8f2f17769f88c534
Savindu Amalka <savinduamalka@gmail.com>
2025-07-30 23:22:33 +0530

    Enhance userRouter to include getCurrentUser route for authenticated users

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, getCurrentUser, loginUser } from "../controllers/userController.js";
+import verifyJWT from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/",createUser)
-userRouter.post("/login",loginUser)
+userRouter.post("/", createUser);
+userRouter.post("/login", loginUser);
+
+
+userRouter.get("/me", verifyJWT, getCurrentUser);
 
 export default userRouter;
