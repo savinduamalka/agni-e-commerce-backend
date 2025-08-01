60f3d758888ae6aae93ef3616a39a8e8eb7f98b1
Savindu Amalka <savinduamalka@gmail.com>
2025-08-01 09:54:05 +0530

    Add security and performance enhancements to the Express app
    - Implemented rate limiting using express-rate-limit to restrict requests.
    - Added helmet for security headers.
    - Integrated CORS for cross-origin resource sharing.
    - Organized userRouter middleware for improved API structure.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './src/.env' });
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import userRouter from './routes/userRoute.js';
 
 const app= express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    standardHeaders: true,
+    legacyHeaders: false, 
+});
+
+app.use(limiter);
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 
 const connection = process.env.MONGODB_URL;
@@ -27,7 +42,5 @@ mongoose.connect(connection).then(
     console.log("Database connection failed");
   }
 )
-
-app.use("/api/users", userRouter);
 
 export default app;
