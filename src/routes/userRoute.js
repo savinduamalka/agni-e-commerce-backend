bdd464f656022dd2af78ea9b12dd9dc5314b4f1b
Savindu Amalka <savinduamalka@gmail.com>
2025-08-01 09:53:05 +0530

    Enhance userRouter to include sendOTP route for email verification and add admin-only access route with JWT verification.

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { createUser, getCurrentUser, loginUser, loginWithGoogle } from "../controllers/userController.js";
-import verifyJWT from "../middleware/auth.js";
+import { createUser, getCurrentUser, loginUser, loginWithGoogle, sendOTP } from "../controllers/userController.js";
+import { verifyJWT, isAdmin } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/login/google", loginWithGoogle);
+userRouter.post("/send-otp", sendOTP);
 
 
 userRouter.get("/me", verifyJWT, getCurrentUser);
+
+userRouter.get("/admin-only", verifyJWT, isAdmin, (req, res) => {
+    res.json({ message: "Welcome, admin!" });
+});
 
 export default userRouter;
