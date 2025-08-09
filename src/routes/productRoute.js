3773f9dca668dfeb872f5e86dfe3aa9eb299c0ab
Savindu Amalka <savinduamalka@gmail.com>
2025-08-09 23:17:43 +0530

    Add route for retrieving all products with admin access

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 const router = Router();
-import { createProduct, getActiveProducts } from '../controllers/productController.js';
+import {
+  createProduct,
+  getActiveProducts,
+  getAllProducts,
+} from '../controllers/productController.js';
 import { verifyJWT, isAdmin } from '../middleware/auth.js';
 
 router.post('/', verifyJWT, isAdmin, createProduct);
 router.get('/', getActiveProducts);
+router.get('/admin/all', verifyJWT, isAdmin, getAllProducts);
 
 export default router;
