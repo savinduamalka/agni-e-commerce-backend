05fc1931f71b5c927c70020dfc61af5a7f65d589
Savindu Amalka <savinduamalka@gmail.com>
2025-08-11 22:33:06 +0530

    Add deleteProduct route with authentication and admin access

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,11 +4,13 @@ import {
   createProduct,
   getActiveProducts,
   getAllProducts,
+  deleteProduct,
 } from '../controllers/productController.js';
 import { verifyJWT, isAdmin } from '../middleware/auth.js';
 
 router.post('/', verifyJWT, isAdmin, createProduct);
 router.get('/', getActiveProducts);
 router.get('/admin/all', verifyJWT, isAdmin, getAllProducts);
+router.delete('/:id', verifyJWT, isAdmin, deleteProduct);
 
 export default router;
