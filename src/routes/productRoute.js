b54c64ab75d5de73c8a8f480144c4ca9f9af37d6
Savindu Amalka <savinduamalka@gmail.com>
2025-08-21 21:49:04 +0530

    Add new product routes for fetching hot products and offers, along with bulk update functionalities

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,12 +5,37 @@ import {
   getActiveProducts,
   getAllProducts,
   deleteProduct,
+  getHotProducts,
+  getOfferProducts,
+  updateProductHotStatus,
+  updateProductOffer,
+  incrementSalesCount,
+  getProductAnalytics,
+  bulkUpdateHotStatus,
+  bulkUpdateOffers,
+  getHotProductsByCategory,
+  getOffersByCategory,
 } from '../controllers/productController.js';
 import { verifyJWT, isAdmin } from '../middleware/auth.js';
 
-router.post('/', verifyJWT, isAdmin, createProduct);
+// Public routes
 router.get('/', getActiveProducts);
+router.get('/hot', getHotProducts);
+router.get('/offers', getOfferProducts);
+router.get('/hot/category/:category', getHotProductsByCategory);
+router.get('/offers/category/:category', getOffersByCategory);
+
+// Admin routes
+router.post('/', verifyJWT, isAdmin, createProduct);
 router.get('/admin/all', verifyJWT, isAdmin, getAllProducts);
 router.delete('/:id', verifyJWT, isAdmin, deleteProduct);
+
+// Product management routes
+router.patch('/:id/hot-status', verifyJWT, isAdmin, updateProductHotStatus);
+router.patch('/:id/offer', verifyJWT, isAdmin, updateProductOffer);
+
+// Bulk operations
+router.patch('/bulk/hot-status', verifyJWT, isAdmin, bulkUpdateHotStatus);
+router.patch('/bulk/offers', verifyJWT, isAdmin, bulkUpdateOffers);
 
 export default router;
