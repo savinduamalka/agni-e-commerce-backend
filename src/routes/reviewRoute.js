0061ef229881248b5d5b5980bb61db17395c2ce6
Savindu Amalka <savinduamalka@gmail.com>
2025-09-03 20:14:13 +0530

    Add getUserReviews route

--- src/routes/reviewRoute.js
+++ src/routes/reviewRoute.js
@@ -5,6 +5,7 @@ import {
   getProductReviews,
   updateReview,
   deleteReview,
+  getUserReviews,
 } from '../controllers/reviewController.js';
 import { verifyJWT } from '../middleware/auth.js';
 
@@ -13,10 +14,12 @@ router.get('/product/:productId', getProductReviews);
 
 // Protected routes (require authentication)
 router.post('/', verifyJWT, createReview);
+router.get('/user', verifyJWT, getUserReviews);
 router.put('/:reviewId', verifyJWT, updateReview);
 router.delete('/:reviewId', verifyJWT, deleteReview);
 
 export default router;
+
 
 
 
