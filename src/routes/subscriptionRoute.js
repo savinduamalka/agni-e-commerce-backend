111789cc5f5df17f5c1c5e2515b8b97adee59ac5
Savindu Amalka <savinduamalka@gmail.com>
2025-09-16 23:04:33 +0530

    Add subscription route with endpoints for user subscription and admin access to subscriber list

---
+++ src/routes/subscriptionRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { subscribe, listSubscribers } from "../controllers/subscriptionController.js";
+import { verifyJWT, isAdmin } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// Public endpoint for users to subscribe by email
+router.post("/subscribe", subscribe);
+
+// Admin-only endpoints
+router.get("/", verifyJWT, isAdmin, listSubscribers);
+
+export default router;
+
+
